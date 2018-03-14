f9c31199875121303c247248dc60f0c38fbed0bc
Sam De Maeyer <sam@showmyhomework.co.uk>
2018-03-14 10:27:00 +0000

    Transition after setting currentUser

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,12 +4,14 @@ import { getOwner } from '@ember/application';
 export default Route.extend({
   beforeModel({ targetName }) {
     this._super(...arguments);
-    if (targetName === 'authenticated.index') {
-      this.transitionTo('authenticated.my-atelier');
-    }
     return this.get('session').fetch().then(() => {
       const userProxy = getOwner(this).lookup('service:current-user');
-      return this.store.findRecord('user', this.get('session.uid')).then(user => userProxy.set('content', user));
+      return this.store.findRecord('user', this.get('session.uid')).then(user => {
+        userProxy.set('content', user);
+        if (targetName === 'authenticated.index') {
+          this.transitionTo('authenticated.my-atelier');
+        }
+      });
     }).catch((error) => {
       if (error.message === 'No session available') {
         this.transitionTo('sign-in')
